1c005757a75d57480bfda858dc418722173c3956
Jesper Dahlberg <jesperdahlberg.jd@gmail.com>
2023-04-19 11:04:38 +0000

    add function structures

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function runGame() {
+
+}
+function checkAnswer() {
+
+}
+function calculateCorrectAnswer() {
+
+}
+function incrementScrore() {
+
+}
+function incrementWrongAnswer() {
+
+}
+function displayAdditionQuestion() {
+
+}
+function displaySubtractQustion() {
+
+}
+function displayMultiplyQuestion() {
+    
+}
